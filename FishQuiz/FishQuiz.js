473dbb5b2e8c6556d59d8726eecc79228d337c97
Fish Dude <rotatingfun@gmail.com>
2026-04-07 19:17:55 -0700

    add noscript to fisquiz

--- FishQuiz/FishQuiz.js
+++ FishQuiz/FishQuiz.js
@@ -1,9 +1,4 @@
-function home() {
-    return (
-        <div>
-            <h1>Welcome To The Fish Quiz!</h1>
-            <button onClick={() => window.location.href = '/quiz'}>Start Quiz</button>
-        </div>
-    );
-}
-document.getElementById('quiz-container').innerHTML = home(); 
+
+const quizContainer = document.getElementById('quiz-container');
+quizContainer.innerHTML = "<div><h1>Welcome To The Fish Quiz!</h1> <button onclick='startQuiz()'>Start Quiz</button>";
+function startQuiz() {}
