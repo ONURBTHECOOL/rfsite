b09adac78d4f0fc78d4413cc63fa4088c8511b89
Fish Dude <rotatingfun@gmail.com>
2026-04-11 16:04:05 -0700

    add intentload() function

--- bald.js
+++ bald.js
@@ -21,3 +21,6 @@ function hair() {
     document.cookie = "baldness=false; path=/";
     document.write("<link rel='stylesheet' href='/stylesheet2.css'><h6>Sorry This Site Is Only For Bald People!</h6>");
 }
+function intentload() {
+    document.write("<link rel='stylesheet' href='/stylesheet.css'><h6>Loading...</h6>");
+}
