6dc8d7fd271912e219d48d2adf6e0565483d8266
Fish Dude <rotatingfun@gmail.com>
2026-04-11 13:15:00 -0700

    nav.js embed

--- FishQuiz/FishQuiz.js
+++ FishQuiz/FishQuiz.js
@@ -2,10 +2,11 @@
 const quizContainer = document.getElementById('quiz-container');
 quizContainer.innerHTML = "<div><h1>Welcome To The Fish Quiz!</h1> <button onclick='startQuiz()'>Start Quiz</button>";
 function startQuiz() {
-    quizContainer.innerHTML = `<script> $(function () { $("#nav-placeholder").load("/nav.html"); }); </script><div id="question-container">
+    quizContainer.innerHTML = `<script src="nav.js"></script><div id="question-container">
     <h2 id="question">What are your opinions on Moose™?</h2>
     <div id="options">button</div>
     <button id="next-btn" onclick="nextQuestion()">Next</button>
-</div><script src="https://code.jquery.com/jquery-3.6.0.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>`;
+</div><script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>`;
     loadQuestion();
 }
