f2bda2d5d1989e32abfafbe05cc4e5e9e798e03f
Fish Dude <rotatingfun@gmail.com>
2026-04-25 07:25:59 -0700

    fish

--- FishQuiz/FishQuiz.js
+++ FishQuiz/FishQuiz.js
@@ -1,12 +1,8 @@
 
-const quizContainer = document.getElementById('quiz-container');
-quizContainer.innerHTML = "<div><h1>Welcome To The Fish Quiz!</h1> <button onclick='startQuiz()'>Start Quiz</button>";
+quizContainer.innerHTML = "<div><h2>Welcome To The Fish Quiz!</h2> <button class='link-button' onclick='startQuiz()'>Start Quiz</button>";
 function startQuiz() {
-    quizContainer.innerHTML = `<script src="/nav.js"></script><div id="question-container">
-    <h2 id="question">What are your opinions on Moose™?</h2>
-    <div id="options">button</div>
-    <button id="next-btn" onclick="nextQuestion()">Next</button>
-</div><script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>`;
-    loadQuestion();
+    quizContainer.innerHTML = "<h2>Question 1:</h2><p>How would you describe a moose?</p><button class='link-button' onclick='document.cookie = 'q1=c; path=/' qtwo()>Amazing/Cool</button><button class='link-button' onclick='document.cookie = 'q1=w; path=/ qtwo()'>Other/Bad</button>"
+}
+function qTwo() {
+    quizContainer.innerHTML = ""
 }
