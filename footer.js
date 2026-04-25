43d0779dbddaaed8cecfd3f059040f81db80a540
Fish Dude <rotatingfun@gmail.com>
2026-04-25 09:29:23 -0700

    oops

--- footer.js
+++ footer.js
@@ -3,11 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     <hr>
     <footer>
       <i>The RF Logo, Rotating Fish, And RF Are trademarks Of The The Rotating Fish Group.</i>
-      <i>Copyright © 2026 Rotating Fish. All rights reserved.</i>
+      <i><a href="/copywrite.txt" alt="copyright" title="copyright">Copyright © 2026 Rotating Fish.</a> All rights reserved.</i>
       <a href="https://github.com/ONURBTHECOOL/rfsite" title="GitHub">
         <p class="link-button">View Source on GitHub</p>
       </a>
-      <p><a href="/copywrite.txt" alt="copyright" title="copyright">More Copyright!</a></p>
     </footer>
   `;
     document.body.insertAdjacentHTML('beforeend', footerHTML);
