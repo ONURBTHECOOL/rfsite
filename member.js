bf3a29c4149dfd004b388995ea9dd5c81c5d7558
Fish Dude <rotatingfun@gmail.com>
2026-05-03 16:34:10 -0700

    hi

---
+++ member.js
@@ -0,0 +1,30 @@
+const usr = {
+    manager="The Balder",
+    co_manager="That Man",
+    presedent="(unknown)",
+    vice_presedent="Dude",
+    head_spy="(unknown)",
+    spy="(unknown)"
+}
+const position = {
+    "The Balder": "manager",
+    "That Man": "co_manager",
+    "Dude": "vice_presedent",
+    "(unknown)": ["presedent", "head_spy", "spy"]
+    //rev. values to be added later
+}
+
+const password = {
+    manager="balder123",
+    co_manager="thatman123",
+    presedent="unknown123",
+    vice_presedent="dude123",
+    head_spy="unknown123",
+    spy="unknown123"
+}
+
+ function login_usr(id, password) {
+    if (id in position) {
+
+    }
+ }
