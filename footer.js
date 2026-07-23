f5578a3692d0002f12883e22b821331c7fd27a33
Fish Dude <rotatingfun@gmail.com>
2026-07-23 15:16:25 -0700

    Update footer license to GPL-3.0-or-later and link to /LICENSE

--- footer.js
+++ footer.js
@@ -1,9 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Rotating Fish Corporation
+
 document.addEventListener('DOMContentLoaded', function () {
     const footerHTML = `
     <hr>
     <footer>
-      <i>The RF Logo, Rotating Fish, And RF Are trademarks Of The Rotating Fish Group.</i>
-      <i><a href="/copywrite.txt" alt="copyright" title="copyright">Copyright © 2026 Rotating Fish.</a> All rights reserved.</i>
+      <i>The RF Logo, Rotating Fish, and RF are trademarks of the Rotating Fish Group.</i><br>
+      <i>This website and its source code are licensed under the GNU General Public License v3 (GPL-3.0-or-later). <a href="/LICENSE" title="License">See /LICENSE</a></i>
       <a href="https://github.com/ONURBTHECOOL/rfsite" title="GitHub">
         <p class="link-button">View Source on GitHub</p>
       </a>
